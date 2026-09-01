0ee3ed7fab47b510b63c7bc8630a5c2765f206e4
CarlosNeto242 <carloslmn@al.insper.edu.br>
2026-09-01 10:30:20 -0300

    Implementando CSS em django

---
+++ notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    id = models.BigAutoField(primary_key=True)
+    def __str__(self):
+        return f"{self.id}.{self.title}"
