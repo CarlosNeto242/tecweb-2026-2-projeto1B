f5c12ac653641757b7388a8a482c19c0236951df
CarlosNeto242 <carloslmn@al.insper.edu.br>
2026-09-15 09:02:35 -0300

    Implementando configurações inciais para o deploy

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
+    id = models.BigAutoField(primary_key=True)
+    
+    def __str__(self):
+        return f"{self.id}.{self.title}"
+
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     id = models.BigAutoField(primary_key=True)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return f"{self.id}.{self.title}"
